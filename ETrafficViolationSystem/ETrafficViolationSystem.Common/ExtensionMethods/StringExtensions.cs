21102364c9c9cb06f2d90e2fc1eb1bbfa3dc44ba
JunaidSultan20 <73248084+JunaidSultan20@users.noreply.github.com>
2021-08-01 02:10:27 +0500

    ETrafficViolationSystem.Common: String Extension Method Added.

---
+++ ETrafficViolationSystem/ETrafficViolationSystem.Common/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ETrafficViolationSystem.Common.ExtensionMethods
+{
+    public static class StringExtensions
+    {
+        public static int ToInt(this string value)
+        {
+            return Convert.ToInt32(value);
+        }
+    }
+}
