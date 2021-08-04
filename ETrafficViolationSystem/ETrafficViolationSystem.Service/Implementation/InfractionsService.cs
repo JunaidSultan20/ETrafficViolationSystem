e75fac0e593dabcbcaffe3c11ee5c1c3af2e48ab
JunaidSultan20 <73248084+JunaidSultan20@users.noreply.github.com>
2021-08-04 21:10:27 +0500

    ETrafficViolationSystem.Service: Car Model & Infractions Service Added.

---
+++ ETrafficViolationSystem/ETrafficViolationSystem.Service/Implementation/InfractionsService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using AutoMapper;
+using ETrafficViolationSystem.Data.UnitOfWork.Interface;
+using ETrafficViolationSystem.Entities.Dto;
+using ETrafficViolationSystem.Entities.Models;
+using ETrafficViolationSystem.Entities.Response;
+using ETrafficViolationSystem.Service.Interface;
+
+namespace ETrafficViolationSystem.Service.Implementation
+{
+    public class InfractionsService : IInfractionsService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IMapper _mapper;
+
+        public InfractionsService(IUnitOfWork unitOfWork, IMapper mapper)
+        {
+            _unitOfWork = unitOfWork;
+            _mapper = mapper;
+        }
+
+        public async Task<BaseResponse<IEnumerable<InfractionsDto>>> GetInfractionsList()
+        {
+            IEnumerable<Infractions> result = await _unitOfWork.Repository<Infractions>().Get(x => x.IsActive);
+            if (result == null && !result.Any())
+                return new BaseResponse<IEnumerable<InfractionsDto>>(HttpStatusCode.NotFound, null);
+            return new BaseResponse<IEnumerable<InfractionsDto>>(HttpStatusCode.OK, null,
+                _mapper.Map<IEnumerable<InfractionsDto>>(result), result.Count());
+        }
+
+        public async Task<BaseResponse<InfractionsDto>> GetInfractionById(int id)
+        {
+            Infractions result = await _unitOfWork.Repository<Infractions>().FindAsync(x => x.InfractionId == id);
+            if (result == null)
+                return new BaseResponse<InfractionsDto>(HttpStatusCode.NotFound, null);
+            return new BaseResponse<InfractionsDto>(HttpStatusCode.OK, null, _mapper.Map<InfractionsDto>(result));
+        }
+    }
+}
