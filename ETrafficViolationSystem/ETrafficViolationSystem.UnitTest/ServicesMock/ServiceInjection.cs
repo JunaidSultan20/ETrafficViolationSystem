40b9e51fb0c2a67a3a9a53abdc0c20d323a0a78f
JunaidSultan20 <73248084+JunaidSultan20@users.noreply.github.com>
2021-11-17 19:44:04 +0500

    ETrafficViolationSystem.UnitTest: Unit Test Project Added.

---
+++ ETrafficViolationSystem/ETrafficViolationSystem.UnitTest/ServicesMock/ServiceInjection.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using ETrafficViolationSystem.Data.Context;
+using ETrafficViolationSystem.Data.UnitOfWork.Implementation;
+using ETrafficViolationSystem.Data.UnitOfWork.Interface;
+using ETrafficViolationSystem.Service.Implementation;
+using ETrafficViolationSystem.Service.Interface;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace ETrafficViolationSystem.UnitTest.ServicesMock
+{
+    public class ServiceInjection
+    {
+        private IConfiguration _configuration;
+
+        public ServiceProvider GetServiceProvider()
+        {
+            ServiceCollection services = new ServiceCollection();
+            IConfigurationBuilder configurationBuilder = new ConfigurationBuilder();
+            //configurationBuilder.SetBasePath(Directory.GetCurrentDirectory());
+            configurationBuilder.AddJsonFile(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.Parent.Parent.FullName +
+                                             "/ETrafficViolationSystem.API/appsettings.json");
+            _configuration = configurationBuilder.Build();
+            services.AddDbContext<ETrafficViolationSystemContext>(options =>
+                options.UseSqlServer(_configuration.GetConnectionString("DefaultConnection")));
+            services.AddHttpContextAccessor();
+            services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+            services.AddTransient<IPropertyMappingService, PropertyMappingService>();
+            services.AddTransient<IInfractionsService, InfractionsService>();
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+            ServiceProvider serviceBuilder = services.BuildServiceProvider();
+            return serviceBuilder;
+        }
+    }
+}
---
+++ ETrafficViolationSystem/ETrafficViolationSystem.UnitTest/ServicesTest/InfractionsServiceTest.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Net;
+using ETrafficViolationSystem.Entities.Request.QueryParameters;
+using ETrafficViolationSystem.Service.Interface;
+using ETrafficViolationSystem.UnitTest.ServicesMock;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+
+namespace ETrafficViolationSystem.UnitTest.ServicesTest
+{
+    public class InfractionsServiceTest
+    {
+        private readonly IInfractionsService _infractionsService;
+
+        public InfractionsServiceTest()
+        {
+            ServiceInjection serviceInjection = new ServiceInjection();
+            _infractionsService = serviceInjection.GetServiceProvider().GetService<IInfractionsService>();
+        }
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void GetInfractionsListTest()
+        {
+            var result = _infractionsService.GetInfractionsList(new BaseQueryParameters
+                { OrderBy = "id asc", PageNumber = 1, PageSize = 10 }).Result;
+            Assert.AreEqual(200, result.Result.FirstOrDefault().Penalty);
+        }
+
+        [Test]
+        public void GetInfractionByIdTest()
+        {
+            var result = _infractionsService.GetInfractionById(20).Result;
+            Assert.AreEqual(HttpStatusCode.OK, result.StatusCode);
+            Assert.AreEqual("Failing to dip head light for other traffic.", result.Result.Description);
+            Assert.AreNotSame(false, result.IsSuccessful);
+        }
+
+        [Test]
+        public void GetByPenaltyTest()
+        {
+            var result = _infractionsService.GetByPenalty(100).Result;
+            Assert.AreEqual(11, result.Result.Count());
+
+            result = _infractionsService.GetByPenalty(200).Result;
+            Assert.AreEqual(26, result.Result.Count());
+        }
+
+        [Test]
+        public void GetByPointsTest()
+        {
+            var result = _infractionsService.GetByPoints(5).Result;
+            Assert.AreEqual(15, result.Result.Count());
+
+            result = _infractionsService.GetByPoints(10).Result;
+            Assert.AreNotEqual(6, result.Result.Count());
+        }
+    }
+}
